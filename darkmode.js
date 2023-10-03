73e8230c98e57b963241ea591a299172ec72f32c
zushii <zxgamingh@gmail.com>
2023-10-03 21:58:58 +0200

    helo

---
+++ darkmode.js
@@ -0,0 +1,21 @@
+const switchTheme = () => {
+  const rootElem = document.documentElement
+  let dataTheme = rootElem.getAttribute('data-theme'),
+      newTheme
+
+  newTheme = (dataTheme === 'light') ? 'dark' : 'light'
+
+  rootElem.setAttribute('data-theme', newTheme)
+
+  localStorage.setItem('theme', newTheme)
+}
+
+document.querySelector('#theme-switcher').addEventListener('click', switchTheme)
+
+const ham_menu = document.querySelector('.hamburger');
+const mob_menu = document.querySelector('.mobile-nav');
+
+ham_menu.addEventListener('click', function () {
+  ham_menu.classList.toggle('is-active');
+  mob_menu.classList.toggle('is-active');
+});
