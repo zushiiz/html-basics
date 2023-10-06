2e99cd9947676a25884ba7607b1f873c1d864bc3
zushii <zxgamingh@gmail.com>
2023-10-06 12:05:16 +0200

    new hamburger menu for html.basics

--- darkmode.js
+++ darkmode.js
@@ -13,7 +13,7 @@ const switchTheme = () => {
 document.querySelector('#theme-switcher').addEventListener('click', switchTheme)
 
 const ham_menu = document.querySelector('.hamburger');
-const mob_menu = document.querySelector('.mobile-nav');
+const mob_menu = document.querySelector('.mobile-menu');
 
 ham_menu.addEventListener('click', function () {
   ham_menu.classList.toggle('is-active');
