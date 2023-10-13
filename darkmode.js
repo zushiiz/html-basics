885c2abb2249de12c42ffd61418860ed35a5a0dc
zushii <zxgamingh@gmail.com>
2023-10-13 12:18:25 +0200

    hamburger menu done

--- darkmode.js
+++ darkmode.js
@@ -19,3 +19,17 @@ ham_menu.addEventListener('click', function () {
   ham_menu.classList.toggle('is-active');
   mob_menu.classList.toggle('is-active');
 });
+
+const mobielswitch = () => {
+  const rootElem = document.documentElement
+  let dataTheme = rootElem.getAttribute('data-theme'),
+      newTheme
+
+  newTheme = (dataTheme === 'light') ? 'dark' : 'light'
+
+  rootElem.setAttribute('data-theme', newTheme)
+
+  localStorage.setItem('theme', newTheme)
+}
+
+document.querySelector('.theme').addEventListener('click', switchTheme)
